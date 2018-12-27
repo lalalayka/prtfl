b9b73ca95d9c48c896a9725924a48876b0509504
lalalayka <30436306+lalalayka@users.noreply.github.com>
2018-12-27 23:50:19 +0200

    Added webfonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,8 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+  <div className="txt__intro">
+    <p>Hello! my name is Sergey. I’m working as communication designer since 2011. I have expertise in creation as online as offline products. Since the end of 2015 I started to gain experience in UI/UX design. In my past, I worked with such companies as Kommersant and Yandex. At this moment I’m employed as a senior graphic/web/UI designer in Exness, one of the top leaders’ forex broker. Located in Cyprus.<br></br>If you are interested in doing some projects with my help or wanna just say «Hi!» or ask something, don’t be shy write me a letter. I would like to help you.</p>
   </div>
 )
 
