fa20c8b1fbd945befe3fb26df4ba54c60573e545
lalalayka <30436306+lalalayka@users.noreply.github.com>
2019-01-06 20:06:18 +0200

    Header Added. Footer WIP

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,20 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './header.css'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+const Header = () => (
+  <div className="container__header">
+    <div className='header'>
+      <div className="header__logo">
+        <a href="../../">
+          <span>Sergey Kovalchuk</span>
+        </a>
+      </div>  
+      <div className="header__navigation">
+        <button className="btn btn--active">ru</button>
+        <button className="btn">en</button>
+      </div>
+    </div>  
   </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,13 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div className="txt__intro">
-    <p>Hello! my name is Sergey. I’m working as communication designer since 2011. I have expertise in creation as online as offline products. Since the end of 2015 I started to gain experience in UI/UX design. In my past, I worked with such companies as Kommersant and Yandex. At this moment I’m employed as a senior graphic/web/UI designer in Exness, one of the top leaders’ forex broker. Located in Cyprus.<br></br>If you are interested in doing some projects with my help or wanna just say «Hi!» or ask something, don’t be shy write me a letter. I would like to help you.</p>
+  <div className="container__main">
+    <div className="txt__intro">
+      <span>
+        <p>Hello! my name is Sergey. I’m working as communication designer since 2011. I have expertise in creation as online as offline products. Since the end of 2015 I started to gain experience in UI/UX design. In my past, I worked with such companies as Kommersant and Yandex. At this moment I’m employed as a senior graphic/web/UI designer in Exness, one of the top leaders’ forex broker. Located in Cyprus.</p>
+        <p>If you are interested in doing some projects with my help or wanna just say «Hi!» or ask something, don’t be shy write me a letter. I would like to help you.</p>
+      </span>
+    </div>
   </div>
 )
 
